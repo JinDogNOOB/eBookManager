11538d5b8361cce48626a33f6d12d114a491d4c3
JinDogNOOB <mrriey00@naver.com>
2020-11-19 17:02:10 +0900

    initial commit

---
+++ eBookManager.Engine/DeweyDecimal.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace eBookManager.Engine{
+    public class DeweyDecimal{
+        public string ComputerScience {get;set;} = "000";
+        public string DataProcessing {get;set;} = "004";
+        public string ComputerProgramming {get;set;} = "005";
+    }
+
+
+}
---
+++ eBookManager.Engine/Document.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace eBookManager.Engine
+{
+    // 책클래스
+    public class Document{
+        public string title{get;set;}
+        public string fileName {get; set;}
+        public string extension {get;set;}
+        public DateTime lastAccessed{get;set;}
+        public DateTime created{get;set;}
+        public string filePath{get;set;}
+        public string fileSize{get;set;}
+        public string ISBN{get;set;}
+        public string price{get;set;}
+        public string publisher{get;set;}
+        public string author{get;set;}
+        public DateTime publishDate{get;set;}
+        public DeweyDecimal classification {get;set;}
+        public string category {get;set;}
+
+    }
+}
